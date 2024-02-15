223b2ff17401cd80e45cab2fe3c69999f11fda90
NaderMoa <nadermoazezi@gmail.com>
2024-02-15 12:34:22 -0500

    bubble chart created

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,10 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 mydata = d3.json(url).then(response => console.log(response))
-// const data = d3.json(url)
-// let samplevalues = Object.values(data.samples[0].sample_values).slice(0,9)
-// let outids = Object.values(data.samples[0].otu_ids).slice(0,9)
-// let hoverover = Object.values(data.sample[0].otu_labels).slice(0,9)
-
-
+// creating the dropdown menue and the horizontal bar chart
 function initi() {
     data = d3.json(url).then(response =>{
         let names= response.names;
-        // console.log(names);
+        
 let selector = d3.select("#selDataset");
 for(let i=0; i <= names.length ; i++){
     selector.append("option").text(names[i]).property("values", names[i])
@@ -17,15 +12,16 @@ for(let i=0; i <= names.length ; i++){
 let sampleName0= names[0];
 
 drawBarChart(sampleName0)
+drawBubbleChart(sampleName0)
 
     });}
 
 function drawBarChart(SampleName){
     data = d3.json(url).then(response =>{
-        //let names= response.names;
+        
         let samplesList = response.samples;
         let dataset= samplesList.filter( sample => sample.id == SampleName)[0];
-        // let outids= dataset.map( record => `OTU ${record.otu_ids}`)
+        
         console.log(dataset.samples)
     let trace1 = {
         x: dataset.sample_values.slice(0,10).reverse(),
@@ -40,17 +36,33 @@ function drawBarChart(SampleName){
 Plotly.newPlot("bar",[trace1])
 })
 } 
-function optionChanged(sampleID){
-    console.log(sampleID)
-    drawBarChart(sampleID);
+
+
+
+// creating the bubble chart
+function drawBubbleChart(SampleName){
+     data = d3.json(url).then(response => {
+let bubbleList = response.samples;
+let dataset = bubbleList.filter(sample => sample.id == SampleName)[0];
+let trace2 ={
+    x: dataset.otu_ids,
+    y: dataset.sample_values,
+    text: dataset.otu_labels,
+    type: 'scatter',
+    mode: 'markers',
+    marker: {
+        size: dataset.sample_values,
+        color: dataset.otu_ids,
+        colorscale: 'Bluered'
+    }
+};
+Plotly.newPlot("bubble",[trace2])
+     })
+
 }
-//     let trace1 = {
-//         x: samplevalues,
-//         y: outids.map(id => `OUT${id}`),
-//         type: "bar",
-//         orientation: "h"
-// };
-// let dataset = [trace1];
-// Plotly.newPlot("#bar",dataset)}
-// console.log(data)
 initi()
+function optionChanged(sampleID){
+    console.log(sampleID);
+    drawBarChart(sampleID);
+    drawBubbleChart(sampleID);
+}
